8571cc144c240313a8bfaaca0ba6b20398d2f339
cyruskong1 <akong@esurance.com>
2019-02-03 15:48:28 -0800

    more practice probs

--- practice_files/ToyProblems.js
+++ practice_files/ToyProblems.js
@@ -27,11 +27,10 @@ var numUniqueEmails = function(emails) {
   var storage = {};
    for(var i = 0; i < emails.length; i++) {
        let formattedEmail = formatEmail(emails[i]);
-       formattedEmail = formattedEmail.slice(0,formattedEmail.indexOf("+"))
-       console.log('formattedEmail', formattedEmail)
+       formattedEmail = formattedEmail.slice(0,formattedEmail.indexOf("+")) + formattedEmail.slice(formattedEmail.indexOf("@"))
        if(!storage.hasOwnProperty(formattedEmail)) {
-           storage[formattedEmail] = formattedEmail;
-           uniqueEmails++;
+          storage[formattedEmail] = formattedEmail;
+           uniqueEmails++; 
        }
    } 
     return uniqueEmails;
@@ -42,13 +41,48 @@ var formatEmail = function(email) {
 	for(var i = 0; i < newEmail.length; i++) {
 		if(newEmail[i] === ".") {
 			newEmail = newEmail.substring(0, i) + newEmail.substring(i+1)
-			console.log('newEmail', newEmail)
  		}
- 		else if(newEmail[i] === "@") {
+ 		if(newEmail[i] === "@") {
+			
  			return newEmail;
  		}
 	}
- return newEmail;
 }
 
-console.log(numUniqueEmails(["test.email+alex@leetcode.com","test.e.mail+bob.cathy@leetcode.com","testemail+david@lee.tcode.com"]))
+//console.log(numUniqueEmails(["test.email+alex@leetcode.com","test.e.mail+bob.cathy@leetcode.com","testemail+david@lee.tcode.com"]))
+
+/* 
+In a 2 dimensional array grid, each value grid[i][j] represents the height of a building located there. We are allowed to increase the height of any number of buildings, by any amount (the amounts can be different for different buildings). Height 0 is considered to be a building as well. 
+
+At the end, the "skyline" when viewed from all four directions of the grid, i.e. top, bottom, left, and right, must be the same as the skyline of the original grid. A city's skyline is the outer contour of the rectangles formed by all the buildings when viewed from a distance. See the following example.
+
+What is the maximum total sum that the height of the buildings can be increased?
+*/
+
+
+const maxIncreaseKeepingSkyline = (grid) => {
+	let largestSum = 0;
+	let northSouthView = [];
+	let eastWestView = [];
+
+	// create eastWestView
+		// find largestnum in each array
+	for(var i = 0; i < grid.length; i++) {
+		eastWestView.push(Math.max.apply(null, grid[i]))
+	}
+
+	//createNorthSouthView
+		//find largestNum in each array's index 
+}
+
+
+
+
+
+
+
+
+
+
+
+
